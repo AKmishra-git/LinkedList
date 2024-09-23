4eb0d65748956a44138ffd16d2e9fb5f3c091eb2
Ayush Mishra <akumarmishra652@gmail.com>
2024-09-23 11:04:43 +0530

    Add files via upload
    here is the code for converting the array into singly linked list..

---
+++ convertingarr2linklist.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+
+    }
+};
+
+Node* conversion(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i =1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr = {2,4,5,3};
+    Node* head = conversion(arr);
+    cout<<head->data<<" "<<head->next;
+
+    
+}
+
